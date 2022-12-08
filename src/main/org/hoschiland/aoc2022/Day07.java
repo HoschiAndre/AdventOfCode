91d99e915581a5b8c5a612cdfe113a68ecd069c3
Andre Kisters <andre.kisters@bmw.de>
2022-12-08 18:09:14 +0100

    Day08

--- src/main/org/hoschiland/aoc2022/Day07.java
+++ src/main/org/hoschiland/aoc2022/Day07.java
@@ -5,7 +5,7 @@ package org.hoschiland.aoc2022;
 
 import org.hoschiland.common.AoCInputReader;
 
-import java.util.List;
+import java.util.*;
 
 
 public class Day07 extends AoCInputReader {
@@ -18,13 +18,152 @@ public class Day07 extends AoCInputReader {
     }
 
     List<String> data = fileToStrLines("input07.txt");
+    Map<String, Integer> mapSize = new HashMap<>();
 
     private void part1() {
 
         System.out.println(data);
+
+        Node root = new Node("/");
+        Node currentNode = root;
+
+        for (String str : data) {
+
+            if (str.startsWith("dir")) {
+                String element = str.substring(4);
+                currentNode.addChild(element);
+            } else if (str.equals("$ cd ..")) {
+                currentNode = currentNode.getParent();
+            } else if (str.startsWith("$ cd")) {
+                String moveElement = str.substring(5);
+                List<Node> childList = currentNode.getChildren();
+
+                for (Node element : childList) {
+                    if (element.getData().equals(moveElement)) {
+                        currentNode = element;
+                    }
+                }
+
+            } else if (str.equals("$ ls")) {
+
+
+            } else if (Character.isDigit(str.charAt(0))) {
+                //Integer size = Integer.parseInt(str.replaceAll("[\\D]", ""));
+                currentNode.addChild(str);
+            }
+
+        }
+        traverse(root);
+        printNode(mapSize);
+
+    }
+
+    private void traverse(Node node) {
+        Integer totalSize = 0;
+
+        List<Node> root = node.getChildren();
+
+        for (Node child : root) {
+
+            //System.out.println(child.getData());
+
+            if (child.isLeaf()) {
+                Integer size = Integer.parseInt(child.getData().replaceAll("[\\D]", ""));
+                //totalSize = totalSize + size;
+                //System.out.println("Dir: " + child.getParent().getData() + " Total Size: " + totalSize);
+                Integer rootCount = 0;
+                Node childGoesUp = child;
+                while (!childGoesUp.isRoot()) {
+                    rootCount = rootCount + size;
+                    childGoesUp = childGoesUp.getParent();
+                }
+
+                addSize(childGoesUp.getData(), rootCount);
+
+            }
+
+            traverse(child);
+        }
+    }
+
+    private void addSize(String parent, Integer size) {
+
+        Integer mapElement = mapSize.get(parent);
+
+        if (mapElement == null || mapElement == 0) {
+            mapSize.put(parent, size);
+        } else {
+            mapElement = mapElement + size;
+            mapSize.replace(parent, mapElement);
+        }
+
+
+    }
+
+    private void printNode(Map map) {
+        System.out.println(map);
     }
 
     private void part2() {
 
+    }
+
+    public class Node {
+        private List<Node> children = new ArrayList<Node>();
+        private Node parent = null;
+        private String data = null;
+
+        public Node(String data) {
+            this.data = data;
+        }
+
+        public Node(String data, Node parent) {
+            this.data = data;
+            this.parent = parent;
+        }
+
+        public List<Node> getChildren() {
+            return children;
+        }
+
+        public void setParent(Node parent) {
+            parent.addChild(this);
+            this.parent = parent;
+        }
+
+        public Node getParent() {
+            return this.parent;
+        }
+
+        public void addChild(String data) {
+            Node child = new Node(data);
+            child.parent = this;
+            this.children.add(child);
+        }
+
+        public void addChild(Node child) {
+            child.parent = this;
+            this.children.add(child);
+        }
+
+        public String getData() {
+            return this.data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public boolean isRoot() {
+            return (this.parent == null);
+        }
+
+        public boolean isLeaf() {
+            return this.children.size() == 0;
+        }
+
+        public void removeParent() {
+            this.parent = null;
+        }
     }
 }
--- src/main/org/hoschiland/aoc2022/Day08.java
+++ src/main/org/hoschiland/aoc2022/Day08.java
@@ -5,6 +5,7 @@ package org.hoschiland.aoc2022;
 
 import org.hoschiland.common.AoCInputReader;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -19,9 +20,123 @@ public class Day08 extends AoCInputReader {
 
     List<String> data = fileToStrLines("input08.txt");
 
+    Integer[][] treeMap = new Integer[data.get(0).length()][data.size()];
+
     private void part1() {
 
         System.out.println(data);
+
+        for (int i = 0; i < data.size(); i++) {
+            String str = data.get(i);
+            for (int j = 0; j < str.length(); j++) {
+                treeMap[i][j] = Character.getNumericValue(str.charAt(j));
+            }
+        }
+
+        //printTreeMap();
+
+        var count = findInteriorTree();
+        System.out.println(count);
+    }
+
+    private Integer findInteriorTree() {
+
+        var highCount = 0;
+
+        for (int i = 0; i < treeMap.length - 1; i++) {
+            for (int j = 0; j < treeMap.length - 1; j++) {
+
+                if(i == 0 || j == 0) {
+                    continue;
+                }
+
+                var count = isTreeTopVisible(i,j) * isTreeRightVisible(i,j) * isTreeButtonVisible(i,j) * isTreeLeftVisible(i,j);
+
+                if (count > highCount) {
+                    highCount = count;
+                }
+            }
+        }
+
+        return highCount;
+    }
+
+    private Integer countEdge() {
+        var length = data.get(0).length();
+        var hight = data.size();
+
+        return length + length + hight + hight - 4;
+
+    }
+
+    private Integer isTreeTopVisible(Integer i, Integer j) {
+        var tree = treeMap[i][j];
+        var count = 0;
+
+        for (int k = i - 1; k >= 0; k--) {
+            var treeTop = treeMap[k][j];
+            if (treeTop >= tree) {
+                count++;
+                return count;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    private Integer isTreeRightVisible(Integer i, Integer j) {
+        var tree = treeMap[i][j];
+        var count = 0;
+
+        for (int k = j + 1; k < treeMap.length; k++) {
+            var treeRight = treeMap[i][k];
+            if (treeRight >= tree ) {
+                count++;
+                return count;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    private Integer isTreeButtonVisible(Integer i, Integer j) {
+        var tree = treeMap[i][j];
+        var count = 0;
+        for (int k = i + 1; k < treeMap.length; k++) {
+            var treeButton = treeMap[k][j];
+            if (treeButton >= tree ) {
+                count++;
+                return count;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    private Integer isTreeLeftVisible(Integer i, Integer j) {
+        var tree = treeMap[i][j];
+        var count = 0;
+
+        for (int k = j - 1; k >= 0; k--) {
+            var treeLeft = treeMap[i][k];
+            if (treeLeft >= tree ) {
+                count++;
+                return count;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    private void printTreeMap() {
+
+        for (int i = 0; i < treeMap.length; i++) {
+            for (int j = 0; j < treeMap.length; j++) {
+                System.out.print(treeMap[i][j] + " ");
+            }
+
+            System.out.println();
+        }
     }
 
     private void part2() {
